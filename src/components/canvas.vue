bd0e42a8ed57603acb29a00ee795bb0b85388062
waveoffire <waveoffire98@gmail.com>
2022-04-26 18:15:06 +0200

    added snake corners

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <canvas id="mainCanv" width="500" height="500"></canvas>
-   <div>Wynik: {{snakeWidth-1}}</div>
-  <div v-if="gameover"> Game over</div>
+    <div>Wynik: {{ snakeWidth - 1 }}</div>
+    <div v-if="gameover">Game over</div>
   </div>
 </template>
 
@@ -13,11 +13,11 @@ export default Vue.extend({
   data() {
     return {
       kierunek: 2,
-      snakeCoords: [{ x: 0, y: 0,r:0 }],
+      snakeCoords: [{ x: 0, y: 0, r: 0 }],
       snakeWidth: 1,
       rand1: 0,
       rand2: 0,
-      gameover:false,
+      gameover: false,
       apple_image: new Image(),
       SnakeHead_image: new Image(),
       SnakeBody_image: new Image(),
@@ -29,128 +29,253 @@ export default Vue.extend({
   },
 
   methods: {
-    createbackground(ctx:CanvasRenderingContext2D) {
+    createbackground(ctx: CanvasRenderingContext2D) {
       ctx.strokeStyle = "rgba(0,0,0,0.3)";
-      this.Dirt_image.src = 'img/Dirt.png';
+      this.Dirt_image.src = "img/Dirt.png";
       for (let i = 0; i <= 500; i += 20) {
         for (let j = 0; j <= 500; j += 20) {
-          
-           ctx.drawImage(this.Dirt_image, i, j);
-           //ctx.strokeRect(i, j, 20, 20);
+          ctx.drawImage(this.Dirt_image, i, j);
+          //ctx.strokeRect(i, j, 20, 20);
         }
       }
     },
     init() {
       console.log("init");
-            this.SnakeHead_image.src = 'img/SnakeHead.png';
-      this.SnakeBody_image.src = 'img/Snake_Body.png';
-      this.SnakeTail_image.src='img/Snake_Tail.png';
-      this.SnakeLeft_image.src='img/left-body.png';
-      this.SnakeRight_image.src='img/right-body.png';
-          this.apple_image.src = 'img/Apple.png';
+      this.SnakeHead_image.src = "img/SnakeHead.png";
+      this.SnakeBody_image.src = "img/Snake_Body.png";
+      this.SnakeTail_image.src = "img/Snake_Tail.png";
+      this.SnakeLeft_image.src = "img/left-body.png";
+      this.SnakeRight_image.src = "img/right-body.png";
+      this.apple_image.src = "img/Apple.png";
 
       this.keyEventHandler();
-      var canvas:HTMLCanvasElement = document.getElementById("mainCanv") as
-                 HTMLCanvasElement;
+      var canvas: HTMLCanvasElement = document.getElementById(
+        "mainCanv"
+      ) as HTMLCanvasElement;
 
-      var ctx:CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+      var ctx: CanvasRenderingContext2D = canvas.getContext(
+        "2d"
+      ) as CanvasRenderingContext2D;
       this.createbackground(ctx);
 
       ctx.fillStyle = "green";
       this.move(ctx);
-      
+
       this.drawberry(ctx);
     },
-    drawberry(ctx:CanvasRenderingContext2D) {
-     
-    
+    drawberry(ctx: CanvasRenderingContext2D) {
       this.rand1 = Math.floor(Math.random() * 25);
       this.rand2 = Math.floor(Math.random() * 25);
-        for(let i =0;this.snakeCoords.length>i;i++){
-         if (
-          this.rand1 * 20== this.snakeCoords[i].x &&
-         this.rand2 * 20== this.snakeCoords[i].y
-        ){
-          this.drawberry(ctx)
+      for (let i = 0; this.snakeCoords.length > i; i++) {
+        if (
+          this.rand1 * 20 == this.snakeCoords[i].x &&
+          this.rand2 * 20 == this.snakeCoords[i].y
+        ) {
+          this.drawberry(ctx);
         }
       }
 
       ctx.fillStyle = "red";
-      
+
       //ctx.fillRect(this.rand1 * 20, this.rand2 * 20, 20, 20);
       ctx.drawImage(this.apple_image, this.rand1 * 20, this.rand2 * 20);
     },
-    drawImageRot(img:HTMLImageElement,x:number,y:number,width:number,height:number,deg:number,ctx:CanvasRenderingContext2D){
-    // Store the current context state (i.e. rotation, translation etc..)
-    ctx.save()
+    drawImageRot(
+      img: HTMLImageElement,
+      x: number,
+      y: number,
+      width: number,
+      height: number,
+      deg: number,
+      ctx: CanvasRenderingContext2D
+    ) {
+      // Store the current context state (i.e. rotation, translation etc..)
+      ctx.save();
 
-    //Convert degrees to radian 
-    var rad = deg * Math.PI / 180;
+      //Convert degrees to radian
+      var rad = (deg * Math.PI) / 180;
 
-    //Set the origin to the center of the image
-    ctx.translate(x + width / 2, y + height / 2);
+      //Set the origin to the center of the image
+      ctx.translate(x + width / 2, y + height / 2);
 
-    //Rotate the canvas around the origin
-    ctx.rotate(rad);
+      //Rotate the canvas around the origin
+      ctx.rotate(rad);
 
-    //draw the image    
-    ctx.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
+      //draw the image
+      ctx.drawImage(img, (width / 2) * -1, (height / 2) * -1, width, height);
 
-    // Restore canvas state as saved from above
-    ctx.restore();
-},
-    drawsnake(ctx:CanvasRenderingContext2D) {
-
-
-
+      // Restore canvas state as saved from above
+      ctx.restore();
+    },
+    drawsnake(ctx: CanvasRenderingContext2D) {
       ctx.fillStyle = "green";
       for (let i = 0; i <= this.snakeWidth - 1; i++) {
-        
-        //ctx.drawImage(this.SnakeHead_image, this.snakeCoords[i].x, this.snakeCoords[i].y); 
-        let stopnie=0
-        if(this.kierunek==2){
-          this.snakeCoords[0].r = 180
-        } else if(this.kierunek==1){
-          this.snakeCoords[0].r =  0}
-          else if(this.kierunek==3){
-          this.snakeCoords[0].r = 270}
-          else if(this.kierunek==4){
-          this.snakeCoords[0].r =  90}
-      if(i==0){
-        
-        this.drawImageRot(this.SnakeHead_image,this.snakeCoords[i].x, this.snakeCoords[i].y,20,20,this.snakeCoords[i].r,ctx);
-        
-        }else if(i==this.snakeWidth-1){
-         this.drawImageRot(this.SnakeTail_image,this.snakeCoords[i].x, this.snakeCoords[i].y,20,20,this.snakeCoords[i].r,ctx)
-        
-        }
-        else{
-                if(this.snakeWidth>1&&this.snakeWidth-1!=i){
-                  if(this.snakeCoords[i].r!=this.snakeCoords[i+1].r){
-                    let r =0
-                     if(this.snakeCoords[i+1].r==180){
-                      r = 270
-                     } else if(this.snakeCoords[i+1].r==0){
-                      r =  180}
-                      else if(this.snakeCoords[i+1].r==270){
-                      r = 0}
-                       else if(this.snakeCoords[i+1].r==90){
-                      r =  90}
+        //ctx.drawImage(this.SnakeHead_image, this.snakeCoords[i].x, this.snakeCoords[i].y);
 
-                     if(this.snakeCoords[i].r!>this.snakeCoords[i+1].r){
-                        this.drawImageRot(this.SnakeLeft_image,this.snakeCoords[i].x, this.snakeCoords[i].y,20,20,r,ctx)
-                    }else{
-                         this.drawImageRot(this.SnakeRight_image,this.snakeCoords[i].x, this.snakeCoords[i].y,20,20,r,ctx)
-
-}
-                  }else{
-                   this.drawImageRot(this.SnakeBody_image,this.snakeCoords[i].x, this.snakeCoords[i].y,20,20,this.snakeCoords[i].r,ctx)
-
+        if (this.kierunek == 2) {
+          this.snakeCoords[0].r = 180;
+        } else if (this.kierunek == 1) {
+          this.snakeCoords[0].r = 0;
+        } else if (this.kierunek == 3) {
+          this.snakeCoords[0].r = 270;
+        } else if (this.kierunek == 4) {
+          this.snakeCoords[0].r = 90;
         }
-        
-        }
-        }
+        if (i == 0) {
+          this.drawImageRot(
+            this.SnakeHead_image,
+            this.snakeCoords[i].x,
+            this.snakeCoords[i].y,
+            20,
+            20,
+            this.snakeCoords[i].r,
+            ctx
+          );
+        } else if (i == this.snakeWidth - 1) {
+          this.drawImageRot(
+            this.SnakeTail_image,
+            this.snakeCoords[i].x,
+            this.snakeCoords[i].y,
+            20,
+            20,
+            this.snakeCoords[i].r,
+            ctx
+          );
+        } else {
+          if (this.snakeWidth > 1 && this.snakeWidth - 1 != i) {
+            if (this.snakeCoords[i].r != this.snakeCoords[i + 1].r) {
+              let r = 0;
+              if (
+                this.snakeCoords[i + 1].r == 90 &&
+                this.snakeCoords[i].r == 0
+              ) {
+                this.drawImageRot(
+                  this.SnakeRight_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  90,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 270 &&
+                this.snakeCoords[i].r == 0
+              ) {
+                this.drawImageRot(
+                  this.SnakeRight_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  180,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 0 &&
+                this.snakeCoords[i].r == 270
+              ) {
+                this.drawImageRot(
+                  this.SnakeRight_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  0,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 180 &&
+                this.snakeCoords[i].r == 270
+              ) {
+                this.drawImageRot(
+                  this.SnakeLeft_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  180,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 180 &&
+                this.snakeCoords[i].r == 90
+              ) {
+                this.drawImageRot(
+                  this.SnakeLeft_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  270,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 0 &&
+                this.snakeCoords[i].r == 90
+              ) {
+                this.drawImageRot(
+                  this.SnakeRight_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  270,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 90 &&
+                this.snakeCoords[i].r == 180
+              ) {
+                this.drawImageRot(
+                  this.SnakeRight_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  0,
+                  ctx
+                );
+              } else if (
+                this.snakeCoords[i + 1].r == 270 &&
+                this.snakeCoords[i].r == 180
+              ) {
+                this.drawImageRot(
+                  this.SnakeLeft_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  0,
+                  ctx
+                );
+              }
+              /* if (this.snakeCoords[i].r != this.snakeCoords[i + 1].r) {
 
+              }   else {
+                this.drawImageRot(
+                  this.SnakeRight_image,
+                  this.snakeCoords[i].x,
+                  this.snakeCoords[i].y,
+                  20,
+                  20,
+                  r,
+                  ctx
+                );
+              } */
+            } else {
+              this.drawImageRot(
+                this.SnakeBody_image,
+                this.snakeCoords[i].x,
+                this.snakeCoords[i].y,
+                20,
+                20,
+                this.snakeCoords[i].r,
+                ctx
+              );
+            }
+          }
+        }
       }
 
       /*  for (let i = 0; i <= this.snakeWidth - 1; i++) {
@@ -171,7 +296,7 @@ export default Vue.extend({
         console.log("3:" + i);
       } */
     },
-    move(ctx:CanvasRenderingContext2D) {
+    move(ctx: CanvasRenderingContext2D) {
       var inter = setInterval(() => {
         ctx.clearRect(0, 0, 500, 500);
         this.createbackground(ctx);
@@ -195,18 +320,23 @@ export default Vue.extend({
         } else if (this.kierunek == 4) {
           this.snakeCoords[0].x += 20;
         }
-      for(let i =1;this.snakeCoords.length>i;i++){
-         if (
-          this.snakeCoords[0].x == this.snakeCoords[i].x &&
-          this.snakeCoords[0].y == this.snakeCoords[i].y
-        ){
-          this.gameover=true;
+        for (let i = 1; this.snakeCoords.length > i; i++) {
+          if (
+            this.snakeCoords[0].x == this.snakeCoords[i].x &&
+            this.snakeCoords[0].y == this.snakeCoords[i].y
+          ) {
+            this.gameover = true;
+          }
         }
-      }
-      
-      if(this.snakeCoords[0].x<0||this.snakeCoords[0].x>=500||this.snakeCoords[0].y<0||this.snakeCoords[0].y>=500){
-         this.gameover=true;
-      }
+
+        if (
+          this.snakeCoords[0].x < 0 ||
+          this.snakeCoords[0].x >= 500 ||
+          this.snakeCoords[0].y < 0 ||
+          this.snakeCoords[0].y >= 500
+        ) {
+          this.gameover = true;
+        }
         if (
           this.snakeCoords[0].x == this.rand1 * 20 &&
           this.snakeCoords[0].y == this.rand2 * 20
@@ -219,8 +349,8 @@ export default Vue.extend({
             r: this.snakeCoords[this.snakeCoords.length - 1].r,
           });
         }
-        if(this.gameover==true){
-          clearInterval(inter)
+        if (this.gameover == true) {
+          clearInterval(inter);
         }
       }, 140);
     },
@@ -233,13 +363,13 @@ export default Vue.extend({
           // Alert the key name and key code on keydown
           console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
 
-          if (code == "ArrowUp" && this.kierunek!=2) {
+          if (code == "ArrowUp" && this.kierunek != 2) {
             this.kierunek = 1;
-          } else if (code == "ArrowDown"&& this.kierunek!=1) {
+          } else if (code == "ArrowDown" && this.kierunek != 1) {
             this.kierunek = 2;
-          } else if (code == "ArrowLeft"&& this.kierunek!=4) {
+          } else if (code == "ArrowLeft" && this.kierunek != 4) {
             this.kierunek = 3;
-          } else if (code == "ArrowRight"&& this.kierunek!=3) {
+          } else if (code == "ArrowRight" && this.kierunek != 3) {
             this.kierunek = 4;
           }
         },
